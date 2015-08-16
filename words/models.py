afff34be48abf3fc578d6e2f79a71c0c22d6f6ff
Murtaza <murtraja@gmail.com>
2015-08-16 21:50:14 +0530

    added words to db

--- words/models.py
+++ words/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+    word = models.CharField(max_length = 25, unique=True, null = False)
+    def __str__(self):
+        return self.word
